4fa21c77c92ac5019e323507fe25452b2472393d
Muhammad Adnan <adnan.ned98@gmail.com>
2025-10-05 19:24:20 +0500

    again commit

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,5 @@
 import os
 import sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
